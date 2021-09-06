8026dab5a431e8bd8e599b0af3d2be97502e345f
Juan Cortés <juan.cortes@mad.vass.es>
2021-09-06 17:18:32 +0200

    chore: configure firebase admin

---
+++ libs/firebaseAdmin.ts
@@ -0,0 +1,12 @@
+import firebaseAdmin from 'firebase-admin';
+
+if (!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert({
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+    }),
+    // databaseURL: 'https://myproject-123.firebaseio.com',
+  });
+}
