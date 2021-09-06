e3e01707df8789345d390904f786896c7769af0e
Juan Cortés <juan.cortes@mad.vass.es>
2021-09-06 17:19:35 +0200

    chore(libs/firebaseClient): update params initializaApp

--- libs/firebaseClient.ts
+++ libs/firebaseClient.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 
 if (typeof window !== 'undefined' && !getApps().length) {
   initializeApp({
-    apiKey: 'APIKEY',
-    authDomain: 'myproject-123.firebaseapp.com',
-    databaseURL: 'https://myproject-123.firebaseio.com',
-    projectId: 'myproject-123',
-    storageBucket: 'myproject-123.appspot.com',
-    messagingSenderId: '123412341234',
-    appId: '1:1234123412341234:web:1234123421342134d',
+    apiKey: 'AIzaSyB4j38FVuw6336v-UUdV8mHB6RtG9rrV5g',
+    authDomain: 'AIzaSyB4j38FVuw6336v-UUdV8mHB6RtG9rrV5g',
+    // databaseURL: 'https://myproject-123.firebaseio.com',
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: 'nextjs-auth-ssr.appspot.com',
+    messagingSenderId: '902801314391',
+    appId: '1:902801314391:web:603e3755ee30d0a3364912',
   });
 
   getAuth().setPersistence({ type: 'SESSION' });
